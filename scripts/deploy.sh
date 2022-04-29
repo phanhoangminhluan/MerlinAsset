f0d2cded5f88b1ac4297930b903d08a883e05ccc
phanhoangminhluan <phanhoangminhluan@gmail.com>
2022-04-29 16:43:40 +0700

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 mvn clean install -o
 
 mvn deploy:deploy-file \
--Durl=file:///Users/luan.phm/engineering/Projects/merlin/merlin-asset/deployed-jar \
--Dfile=./target/merlin-asset-0.0.8-SNAPSHOT.jar \
+-Durl=file://deployed-jar \
+-Dfile=./target/merlin-asset-0.0.9-SNAPSHOT.jar \
 -DgroupId=com.merlin \
 -DartifactId=merlin-asset \
 -Dpackaging=jar \
--Dversion=0.0.8-SNAPSHOT
+-Dversion=0.0.9-SNAPSHOT
